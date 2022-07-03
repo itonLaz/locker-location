7b95fcb7267cdc2e39bbccf08b5c6b96aaf45726
ItonLaz <dev.melitonlazaro@gmail.com>
2022-07-04 07:53:16 +0800

    Additional roles and permissions + Member account

--- database/seeders/PermissionTableSeeder.php
+++ database/seeders/PermissionTableSeeder.php
@@ -28,6 +28,15 @@ class PermissionTableSeeder extends Seeder
         Permission::create(['name' => 'create booking']);
         Permission::create(['name' => 'view report']);
         Permission::create(['name' => 'export report']);
+        Permission::create(['name' => 'add roles']);
+        Permission::create(['name' => 'view roles']);
+        Permission::create(['name' => 'edit roles']);
+        Permission::create(['name' => 'delete roles']);
+        Permission::create(['name' => 'add permissions']);
+        Permission::create(['name' => 'view permissions']);
+        Permission::create(['name' => 'edit permissions']);
+        Permission::create(['name' => 'delete permissions']);
+
         
         //Create Admin Role and Apply Roles
         $admin_role = Role::create(['name' => 'admin']);
@@ -38,10 +47,14 @@ class PermissionTableSeeder extends Seeder
         $admin_role->givePermissionTo('create booking');
         $admin_role->givePermissionTo('view report');
         $admin_role->givePermissionTo('export report');
-
-
-        $super_admin_role = Role::create(['name' => 'Super Admin']);
-        //No need to add permission since it is already set via Gate::before, see AuthServiceProvider
+        $admin_role->givePermissionTo('add roles');
+        $admin_role->givePermissionTo('edit roles');
+        $admin_role->givePermissionTo('view roles');
+        $admin_role->givePermissionTo('delete roles');
+        $admin_role->givePermissionTo('add permissions');
+        $admin_role->givePermissionTo('edit permissions');
+        $admin_role->givePermissionTo('view permissions');
+        $admin_role->givePermissionTo('delete permissions');
 
         $admin = User::create([
             'name' => 'Admin Admin', 
@@ -52,6 +65,29 @@ class PermissionTableSeeder extends Seeder
             'updated_at' => now()
         ]);
         $admin->assignRole($admin_role);
+
+
+        $member_role = Role::create(['name' => 'member']);
+        $member_role->givePermissionTo('create booking');
+        $member_role->givePermissionTo('view report');
+        $member_role->givePermissionTo('view roles');
+        $member_role->givePermissionTo('view permissions');
+
+
+
+        $member = User::create([
+            'name' => 'Member',
+            'email' => 'member@argon.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('secret'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        $member->assignRole($member_role);
+        
+
+        $super_admin_role = Role::create(['name' => 'Super Admin']);
+        //No need to add permission since it is already set via Gate::before, see AuthServiceProvider
 
         $super_admin = User::create([
             'name' => 'Super Admin', 
